17c89e326d3ab9da57d9c12f8312958e8b23f251
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-01-20 12:39:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 }
 variable "ami_id" {
   description = "AMI for Amazonlinux Ec2 instance"
-  default     = "ami-090e0fc566929d98b"
+  default     = "ami-0c0b74d29acd0cd97"
 }
 
 
