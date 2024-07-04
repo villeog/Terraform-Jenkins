49974b194fa42fd847d7cdc42b151efa13d9ca6d
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-07-04 21:22:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "testab123"
+  default     = "project"
 }
 
 variable "instance_type" {
