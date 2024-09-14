d99e23fbe4196efce5ecc0ef840255154b453b6c
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-09-14 21:11:55 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "project"
+  default     = "vpc"
 }
 
 variable "instance_type" {
@@ -24,7 +24,7 @@ variable "tag_name" {
 }
 variable "ami_id" {
   description = "AMI for Amazonlinux Ec2 instance"
-  default     = "ami-05295b6e6c790593e"
+  default     = "ami-0182f373e66f89c85"
 }
 
 
